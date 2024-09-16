213567ffa5ab85d8f657dfd1135319caea92af9d
ureyx5 <77127463+ureyx6@users.noreply.github.com>
2024-09-15 17:52:10 -0700

    made this file parent route added Navbar element

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
+import { Outlet } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-      </div>
-      
+    <Navbar/>
+    <Outlet/>
     </>
   )
 }
