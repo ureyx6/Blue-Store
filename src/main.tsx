f1c6531426de6ee128fe75394a5c702564a41280
ureyx5 <77127463+ureyx6@users.noreply.github.com>
2024-09-15 17:50:53 -0700

    added routing for multiple pages made App.tsx parent route

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Home from "./Home.tsx";
+import Home from "./routes/Home.tsx";
+import About from './routes/About.tsx';
+import Contact from './routes/Contact.tsx';
 import App from './App.tsx'
+import Shop from './routes/Shop.tsx';
 import './index.css'
-import { createBrowserRouter, RouterProvider, } from "react-router-dom";
-const router = createBrowserRouter ([
-  {
-    path: "/",
-    element: <Home/>
-  },
-])
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route,} from "react-router-dom";
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path = "/" element = {<App/>}>
+      <Route path ="/" element = {<Home/>}/>
+      <Route path ="/shop" element = {<Shop/>}/>
+      <Route path = "/about" element = {<About/>}/>
+      <Route path ="/contact" element = {<Contact/>}/>
+
+    </Route>
+  )
+)
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router = {router} />
