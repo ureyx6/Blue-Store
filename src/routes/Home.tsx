77e53ea65dfb5bf5ed325d0884847145318f6c9b
ureyx5 <77127463+ureyx6@users.noreply.github.com>
2024-09-15 20:49:22 -0700

    added styling for home page right side wip

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import './Home.css';
+import Button from '../components/Button';
 
 function Home() {
   return (
     <div className ="home">
-      Home
+      <div className = "home-left">
+        <div className = "textbox">
+      Ready to experience endless deals?
+      </div>
+      <div className ="buttonbox">
+        <Button buttonText="Shop Now"/>
+      </div>
+      </div>
+      <div className ="home-right">
+        <img className ="home-image" src="https://m.media-amazon.com/images/I/41bIlvE1rdL._AC_SX679_.jpg"></img>
+        <img className = "home-image" src="https://m.media-amazon.com/images/I/61hZrbHRMWL._AC_SX679_.jpg"></img>
+      </div>
     </div>
   )
 }
