8b35c42727e44d2d82a17f981237ecc5a3784899
ureyx5 <77127463+ureyx6@users.noreply.github.com>
2024-09-15 20:45:34 -0700

    made reuseable button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Button.css"
+
+function Button({buttonText}) {
+  return (
+<button className="button-28" role="button">{buttonText}</button>
+  )
+}
+
+export default Button
