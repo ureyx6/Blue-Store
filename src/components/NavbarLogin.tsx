5615f73fa3bd990db8daf2edfc023f636039e492
ureyx5 <77127463+ureyx6@users.noreply.github.com>
2024-09-17 00:18:58 -0700

    added Outlet to render child elements

--- src/components/NavbarLogin.tsx
+++ src/components/NavbarLogin.tsx
@@ -1,14 +1,18 @@
 import { IoBagCheck } from "react-icons/io5";
 import './NavbarLogin.css'
+import { Outlet } from "react-router-dom";
 
 function NavbarLogin() {
     return (
-      <nav className ="navbar">
+        <>
+      <nav className ="navbar-login">
           <div className ="navbar-center">
               <a href="/" className ="logo"><IoBagCheck></IoBagCheck></a>
               <a href="/" className ="logo">BlueStore</a>
           </div>
         </nav>
+        <Outlet/>
+        </>
     )
 }
 
