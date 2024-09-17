ebe600a289c3cea41bd0426a5194c0955e707441
ureyx5 <77127463+ureyx6@users.noreply.github.com>
2024-09-16 23:33:52 -0700

    added routing for login pages with navbarlogin parent route for all login pages

--- src/main.tsx
+++ src/main.tsx
@@ -6,14 +6,19 @@ import App from './App.tsx'
 import Shop from './routes/Shop.tsx';
 import './index.css'
 import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route,} from "react-router-dom";
+import NavbarLogin from './components/NavbarLogin.tsx';
 const router = createBrowserRouter(
   createRoutesFromElements(
+    <>
     <Route path = "/" element = {<App/>}>
       <Route path ="/" element = {<Home/>}/>
       <Route path ="/shop" element = {<Shop/>}/>
       <Route path ="/contact" element = {<Contact/>}/>
 
     </Route>
+    <Route path = "/login" element = {<NavbarLogin/>}>
+    </Route>
+    </>
   )
 )
 createRoot(document.getElementById('root')!).render(
