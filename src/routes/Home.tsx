a2c6105ed2b5b3ae825048c2681912356d6bc3ac
Chris Whitley <ureyx6@gmail.com>
2024-09-16 20:49:31 -0700

    changed homepage into a much cleaner design

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,22 +1,36 @@
 import React from 'react';
 import './Home.css';
 import Button from '../components/Button';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
     <div className ="home">
-      <div className = "home-left">
+      <div className = "home-top">
         <div className = "textbox">
       Ready to experience endless deals?
       </div>
       <div className ="buttonbox">
+        <Link to="/shop">
         <Button buttonText="Shop Now"/>
+        </Link>
       </div>
       </div>
-      <div className ="home-right">
-        <img className ="home-image" src="https://m.media-amazon.com/images/I/41bIlvE1rdL._AC_SX679_.jpg"></img>
-        <img className = "home-image" src="https://m.media-amazon.com/images/I/61hZrbHRMWL._AC_SX679_.jpg"></img>
-        <img className = "home-image" src="https://m.media-amazon.com/images/I/71-D1xCuVwL._AC_SX569_.jpg"></img>
+      <div className = "home-bottom">
+        <div className = "firstbox">
+          <div className = "firstboxheader">
+           All Your Tech Needs
+          </div>
+          <div className = "firstboxtext"> </div>
+        </div>
+        <div className = "secondbox">
+          <div className = "secondboxheader"> Unofficial Retailer Of </div>
+          <div className = "secondboximages">
+            <img className ="logo" src="https://imgs.search.brave.com/1bhL6ufZamxoV5Ikhs9IXvQoGD4DPumk9Nh2polNLQ8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly8xMDAw/bG9nb3MubmV0L3dw/LWNvbnRlbnQvdXBs/b2Fkcy8yMDE2LzEw/L0FwcGxlX2xvZ29f/Z3JleS02MjR4NDAw/LnBuZw"></img>
+            <img className ="logo" src="https://imgs.search.brave.com/C5amKpPS1s9CLpv8ZmGwoStJ3DiBHhHjUwM8dkw609U/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9hc3Nl/dHMuc3RpY2twbmcu/Y29tL2ltYWdlcy81/ODBiNTdmY2Q5OTk2/ZTI0YmM0M2M1MzMu/cG5n"></img>
+            <img className ="logo" src="https://imgs.search.brave.com/VlPYwQnFdWo7MPu8a8wesFMRZem4xYJhR7FGSlTLEQw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly8xMDAw/bG9nb3MubmV0L3dw/LWNvbnRlbnQvdXBs/b2Fkcy8yMDE2LzEw/L0FuZHJvaWQtTG9n/by0yMDE5LTUwMHgz/MTMucG5n"></img>
+          </div>
+        </div>
       </div>
     </div>
   )
